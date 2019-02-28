f69506ccce24fd65bc0db4eeefe663b4cedcbdb6
Herbert Poul <herbert@poul.at>
2019-02-28 03:15:50 +0200

    travis: fix git ssh command for deploy

--- tools/gh-pages-deploy.sh
+++ tools/gh-pages-deploy.sh
@@ -11,17 +11,24 @@ tmpdir=tmp
 rm -rf "$tmpdir"
 mkdir "$tmpdir"
 
-GIT_SSH_COMMAND='ssh -i secrets/github-pages_id_rsa' git clone -b gh-pages https://github.com/${TRAVIS_REPO_SLUG} "$tmpdir"
+rsa=`pwd`/secrets/github-pages_id_rsa
+chmod 600 $rsa
 
-rm -rf "${tmpdir}/public"
-cp -a ${repodir}/public "${tmpdir}/public"
+GIT_SSH_COMMAND="ssh -v -i $rsa" git clone -b gh-pages git@github.com:${TRAVIS_REPO_SLUG} "$tmpdir"
+
+# clean directory...
+rm -rf "${tmpdir}/*"
+# remove all remaining . files (.htaccess), but keep .git sub directory.
+#find "${tmpdir}" -type f -exec rm {} \;
+
+cp -a ${repodir}/public/ "${tmpdir}"
 
 pushd "${tmpdir}"
 
 git status
 git add .
 git commit --author="Travis <ci@travis>" -m "new publish from travis ci ${TRAVIS_BUILD_NUMBER} ${TRAVIS_BUILD_WEB_URL}"
-GIT_SSH_COMMAND='ssh -i secrets/github-pages_id_rsa' git push
+GIT_SSH_COMMAND="ssh -v -i $rsa" git push
 
 popd
 

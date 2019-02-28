efe10b79d2b4381e4822dd697b72f613ef9202c5
Herbert Poul <herbert@poul.at>
2019-02-28 02:50:44 +0200

    fix deploy

--- tools/gh-pages-deploy.sh
+++ tools/gh-pages-deploy.sh
@@ -12,9 +12,9 @@ mkdir "$tmpdir"
 GIT_SSH_COMMAND='ssh -i secrets/github-pages_id_rsa' git clone -b gh-pages https://github.com/${TRAVIS_REPO_SLUG} "$tmpdir"
 
 rm -rf "${tmpdir}/public"
-cp -a ../public "${tmpdir}/public"
+cp -a ./public "${tmpdir}/public"
 
-pushd public
+pushd "${tmpdir}/public"
 
 git add .
 git commit --author="ci@travis" -m "new publish from travis ci ${TRAVIS_BUILD_NUMBER} ${TRAVIS_BUILD_WEB_URL}"

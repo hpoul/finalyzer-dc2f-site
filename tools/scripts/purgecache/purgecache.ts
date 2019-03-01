e99ed9997e1dc54f36cc12db68789bc51adb183e
Herbert Poul <herbert@poul.at>
2019-03-01 16:00:46 +0200

    added post deploy to clean cloudflare cache.

---
+++ tools/scripts/purgecache/purgecache.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as cloudflare from 'cloudflare';
+
+// $ export CF_API_EMAIL='user@example.com'
+// $ export CF_API_KEY='00000000000000000000000000000000'
+
+interface JsonSitemap {
+    pages: [
+        {
+            url: string;
+            lastmod: string;
+        }
+        ];
+}
+
+const secrets = JSON.parse(fs.readFileSync('tools/secrets/purgecache.secrets.json').toString());
+
+const apiEmail = secrets.CF_API_EMAIL;
+const apiKey = secrets.CF_API_KEY;
+const apiZoneId = secrets.CF_ZONE_ID;
+
+if (!apiEmail || !apiKey || !apiZoneId) {
+    console.error('CF_API_EMAIL, CF_API_KEY and CF_ZONE_ID variables must be set in purgecache.secrets.json.');
+    process.exitCode = 1;
+    process.exit(1);
+}
+
+function extractUrls(jsonSitemap: string) {
+    const sitemap: JsonSitemap = JSON.parse(fs.readFileSync(jsonSitemap).toString());
+    return sitemap.pages.map((p) => p.url);
+}
+
+const urls = extractUrls('public/allsites.json');
+
+console.log('clearing cache for urls: ', urls);
+
+const cf = cloudflare({email: apiEmail, key: apiKey});
+cf.zones.purgeCache(apiZoneId, { files: urls }).then((success) => {
+    console.log('Successfully purged cache.', success);
+}).catch((error) => {
+    console.error('error while purging cache.', error);
+    process.exit(1);
+});
+
